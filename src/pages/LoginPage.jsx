09948ce68fb86ce03b53d3afb1d3867ea3f3328a
Aldas Lebeda <aldaslebeda@Aldass-MacBook-Air.local>
2025-04-16 15:05:46 +0300

    Pakeista rakto ikonėlės spalva, kad būtų geriau matoma

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -97,7 +97,7 @@ const LoginPage = () => {
           <form onSubmit={handleSubmit} className="space-y-6">
             <div className="relative">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg className="w-5 h-5 text-gray-400 dark:text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <svg className="w-5 h-5 text-teal dark:text-skyblue" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
                 </svg>
               </div>
