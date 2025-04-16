952d3a8cc4c481e2f62816e474d0c59fbe7e556a
Aldas Lebeda <aldaslebeda@Aldass-MacBook-Air.local>
2025-04-16 18:23:07 +0300

    Pakeista rakto ikona į spynos ikoną su juoda spalva

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -99,7 +99,7 @@ const LoginPage = () => {
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg 
                   className="w-5 h-5" 
-                  style={{color: '#0ea5e9'}} 
+                  style={{color: '#000000'}} 
                   fill="none" 
                   stroke="currentColor" 
                   viewBox="0 0 24 24"
@@ -108,7 +108,7 @@ const LoginPage = () => {
                     strokeLinecap="round" 
                     strokeLinejoin="round" 
                     strokeWidth="2" 
-                    d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" 
+                    d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" 
                   />
                 </svg>
               </div>
